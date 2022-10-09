96fe380f363a887ee9c2523d02f11120ec51c505
Imomali <imomali.msk@gmail.com>
2022-10-09 17:43:00 +0300

    updated impelementation of laptop characteristics

--- sample/generator.go
+++ sample/generator.go
@@ -1,24 +1,29 @@
 package sample
 
-import "github.com/Imomali1/gRPC/pcbook/pb"
+import (
+	"github.com/Imomali1/gRPC/pcbook/pb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
 
 // NewKeyboard returns a new keyboard sample
 func NewKeyboard() *pb.Keyboard {
 	keyboard := &pb.Keyboard{
-		Layout:  RandKeyboardLayout(),
-		Backlit: RandBool(),
+		Layout:  randKeyboardLayout(),
+		Backlit: randBool(),
 	}
+
 	return keyboard
 }
 
 // NewCPU returns a new CPU sample
 func NewCPU() *pb.CPU {
-	brand := RandCPUBrand()
-	name := RandCPUName(brand)
-	numCores := RandInt(2, 8)
-	numThreads := RandInt(numCores, 12)
-	minFreq := RandFloat(2.0, 5.0)
-	maxFreq := RandFloat(minFreq, 8.0)
+	brand := randCPUBrand()
+	name := randCPUName(brand)
+	numCores := randInt(2, 8)
+	numThreads := randInt(numCores, 12)
+	minFreq := randFloat64(2.0, 5.0)
+	maxFreq := randFloat64(minFreq, 8.0)
+
 	cpu := &pb.CPU{
 		Brand:         brand,
 		Name:          name,
@@ -27,5 +32,103 @@ func NewCPU() *pb.CPU {
 		MinFreq:       minFreq,
 		MaxFreq:       maxFreq,
 	}
+
 	return cpu
+}
+
+// NewGPU returns a new GPU sample
+func NewGPU() *pb.GPU {
+	brand := randGPUBrand()
+	name := randGPUName(brand)
+	minFreq := randFloat64(1.0, 1.5)
+	maxFreq := randFloat64(minFreq, 2.0)
+
+	memory := &pb.Memory{
+		Value: uint64(randInt(2, 6)),
+		Unit:  pb.Memory_GIGABYTE,
+	}
+
+	gpu := &pb.GPU{
+		Brand:   brand,
+		Name:    name,
+		MinFreq: minFreq,
+		MaxFreq: maxFreq,
+		Memory:  memory,
+	}
+
+	return gpu
+}
+
+// NewRAM returns a new RAM sample
+func NewRAM() *pb.Memory {
+	ram := &pb.Memory{
+		Value: uint64(randInt(4, 64)),
+		Unit:  pb.Memory_GIGABYTE,
+	}
+
+	return ram
+}
+
+// NewSSD returns a new SSD storage sample
+func NewSSD() *pb.Storage {
+	ssd := &pb.Storage{
+		Driver: pb.Storage_SSD,
+		Memory: &pb.Memory{
+			Value: uint64(randInt(128, 1024)),
+			Unit:  pb.Memory_GIGABYTE,
+		},
+	}
+
+	return ssd
+}
+
+// NewHDD returns a new HDD storage sample
+func NewHDD() *pb.Storage {
+	hdd := &pb.Storage{
+		Driver: pb.Storage_HDD,
+		Memory: &pb.Memory{
+			Value: uint64(randInt(1, 6)),
+			Unit:  pb.Memory_TERABYTE,
+		},
+	}
+
+	return hdd
+}
+
+// NewScreen returns a new screen sample
+func NewScreen() *pb.Screen {
+	screen := &pb.Screen{
+		SizeInch:   randFloat32(13, 17),
+		Resolution: randScreenResolution(),
+		Panel:      randScreenPanel(),
+		MultiTouch: randBool(),
+	}
+
+	return screen
+}
+
+// NewLaptop returns a new laptop sample
+func NewLaptop() *pb.Laptop {
+	brand := randLaptopBrand()
+	name := randLaptopName(brand)
+
+	laptop := &pb.Laptop{
+		Id:       randID(),
+		Brand:    brand,
+		Name:     name,
+		Cpu:      NewCPU(),
+		Ram:      NewRAM(),
+		Gpus:     []*pb.GPU{NewGPU()},
+		Storages: []*pb.Storage{NewSSD(), NewHDD()},
+		Screen:   NewScreen(),
+		Keyboard: NewKeyboard(),
+		Weight: &pb.Laptop_WeightKg{
+			WeightKg: randFloat64(1.0, 3.0),
+		},
+		PriceUsd:    randFloat64(1000, 2500),
+		ReleaseYear: uint32(randInt(2010, 2022)),
+		UpdatedAt:   timestamppb.Now(),
+	}
+
+	return laptop
 }
--- sample/random.go
+++ sample/random.go
@@ -2,10 +2,11 @@ package sample
 
 import (
 	"github.com/Imomali1/gRPC/pcbook/pb"
+	"github.com/google/uuid"
 	"math/rand"
 )
 
-func RandKeyboardLayout() pb.Keyboard_Layout {
+func randKeyboardLayout() pb.Keyboard_Layout {
 	switch rand.Intn(3) {
 	case 1:
 		return pb.Keyboard_QWERTY
@@ -16,13 +17,13 @@ func RandKeyboardLayout() pb.Keyboard_Layout {
 	}
 }
 
-func RandCPUBrand() string {
-	return randomStringFromSet("Intel", "AMD")
+func randCPUBrand() string {
+	return randStringFromSet("Intel", "AMD")
 }
 
-func RandCPUName(brand string) string {
+func randCPUName(brand string) string {
 	if brand == "Intel" {
-		return randomStringFromSet(
+		return randStringFromSet(
 			"XEON E-2286M",
 			"Core i9-1010HK",
 			"Core i7-8328H",
@@ -30,7 +31,7 @@ func RandCPUName(brand string) string {
 			"Core i3-5535W",
 		)
 	} else {
-		return randomStringFromSet(
+		return randStringFromSet(
 			"Ryzen 7 PRO 2700U",
 			"Ryzen 5 PRO 3113E",
 			"Ryzen 3 PRO 3242W",
@@ -38,7 +39,93 @@ func RandCPUName(brand string) string {
 	}
 }
 
-func randomStringFromSet(s ...string) string {
+func randGPUBrand() string {
+	return randStringFromSet("NVIDIA", "AMD")
+}
+
+func randGPUName(brand string) string {
+	if brand == "NVIDIA" {
+		return randStringFromSet(
+			"RTX 2060",
+			"RTX 2070",
+			"GTX 1660-Ti",
+			"GTX 1070",
+		)
+	} else {
+		return randStringFromSet(
+			"RX 590",
+			"RX 580",
+			"RX 5700-XT",
+			"RX Vega-56",
+		)
+	}
+}
+
+func randScreenResolution() *pb.Screen_Resolution {
+	height := randInt(1080, 4320)
+	width := height * 16 / 9
+
+	resolution := &pb.Screen_Resolution{
+		Height: uint32(height),
+		Width:  uint32(width),
+	}
+
+	return resolution
+}
+
+func randScreenPanel() pb.Screen_Panel {
+	if rand.Intn(2) == 1 {
+		return pb.Screen_IPS
+	}
+	return pb.Screen_OLED
+}
+
+func randID() string {
+	return uuid.New().String()
+}
+
+func randLaptopBrand() string {
+	return randStringFromSet("HP", "Acer", "Lenovo", "Apple")
+}
+
+func randLaptopName(brand string) string {
+	switch brand {
+	case "HP":
+		return randStringFromSet(
+			"DragonFly G3",
+			"NB-9007",
+			"Pavilion Plus",
+			"Victus 15",
+			"Omen 16",
+		)
+	case "Acer":
+		return randStringFromSet(
+			"Predator Triton 500",
+			"Spin 5",
+			"Chromebook C933T",
+			"Swift 3x",
+			"Aspire 5",
+		)
+	case "Lenovo":
+		return randStringFromSet(
+			"Legion Slim",
+			"V14 G2",
+			"IdeaPad",
+			"ThinkPad",
+			"Yoga 7",
+		)
+	case "Apple":
+		return randStringFromSet(
+			"MacBook Pro",
+			"MacBook Air",
+			"iMac",
+			"M1 Pro",
+		)
+	}
+	return ""
+}
+
+func randStringFromSet(s ...string) string {
 	n := len(s)
 	if n == 0 {
 		return ""
@@ -46,14 +133,18 @@ func randomStringFromSet(s ...string) string {
 	return s[rand.Intn(n)]
 }
 
-func RandBool() bool {
+func randBool() bool {
 	return rand.Intn(2) == 1
 }
 
-func RandInt(min, max int) int {
+func randInt(min, max int) int {
 	return min + rand.Intn(max-min+1)
 }
 
-func RandFloat(min, max float64) float64 {
+func randFloat64(min, max float64) float64 {
 	return min + rand.Float64()*(max-min)
+}
+
+func randFloat32(min, max float32) float32 {
+	return min + rand.Float32()*(max-min)
 }
