6fa28790d07cc36679c92375ddbc39caf9096b38
Imomali <imomali.msk@gmail.com>
2022-10-15 16:33:22 +0300

    updated project

---
+++ serializer/file.go
@@ -0,0 +1,22 @@
+package serializer
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"os"
+)
+
+// WriteProtoBufToBin writes protocol buffer message to binary file
+func WriteProtoBufToBin(message proto.Message, filename string) error {
+	data, err := proto.Marshal(message)
+	if err != nil {
+		return fmt.Errorf("cannot marshal proto message to binary:%w", err)
+	}
+	
+	err = os.WriteFile(filename, data, 0644)
+	if err != nil {
+		return fmt.Errorf("cannot write binary data to file:%w", err)
+	}
+
+	return nil
+}
